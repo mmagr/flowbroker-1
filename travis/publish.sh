2e3bf555f13ec068c87a28e4ccb16d80257704ba
Matheus Magalhaes <m.magalhaes.ic@gmail.com>
2018-05-04 09:34:42 -0300

    Changes default smtp port

--- travis/publish.sh
+++ travis/publish.sh
@@ -4,7 +4,7 @@ version="latest"
 if [ ${TRAVIS_BRANCH} != "master" ] ; then
   version=${TRAVIS_BRANCH}
 fi
-tag="dojot/flowbroker:${version}"
+tag="${TRAVIS_REPO_SLUG}:${version}"
 
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker tag dojot/flowbroker ${tag}
